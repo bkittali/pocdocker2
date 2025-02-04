ac255c7d5b75eff1c601c9d90c406054fc1dab4a
Basavaraj Kittali <basavaraj.kittali@gardencity.games>
2025-02-04 17:34:53 +0530

    fix

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,6 +1,40 @@
+resource "aws_launch_template" "ecs_launch_template" {
+  name_prefix   = "ecs-launch-template"
+  image_id      = "ami-001eed247d2135475"  # Replace with your correct AMI ID
+  instance_type = "t3.medium"
+  key_name      = "won_ls_key.pem"  # Replace with your SSH key name
+
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.ecs_sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ECS-Instance"
+    }
+  }
+}
+
 resource "aws_autoscaling_group" "ecs_asg" {
   desired_capacity     = 2
   max_size            = 4
   min_size            = 1
   vpc_zone_identifier = [aws_subnet.public_subnet.id]
+
+  launch_template {
+    id      = aws_launch_template.ecs_launch_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ECS-ASG-Instance"
+    propagate_at_launch = true
+  }
 }
