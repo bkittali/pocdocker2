8146ec226fa68c0dced6c2524348cbaac3f7d65e
Basavaraj Kittali <basavaraj.kittali@gardencity.games>
2025-02-05 16:16:30 +0530

    Adding iam role for secrets

--- bitbucket/terraform/iam.tf
+++ bitbucket/terraform/iam.tf
@@ -58,6 +58,28 @@ resource "aws_iam_role_policy_attachment" "jenkins_attach_s3" {
   policy_arn = aws_iam_policy.s3_read_only.arn
 }
 
+resource "aws_iam_policy" "secretsmanager_read" {
+  name        = "SecretsManagerRead"
+  description = "Allows EC2 to read AWS Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "secretsmanager:GetSecretValue",
+        Resource = "arn:aws:secretsmanager:us-east-1:123456789012:secret:jenkins-aws-keys"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+  role       = aws_iam_role.jenkins_ec2_role.name
+  policy_arn = aws_iam_policy.secretsmanager_read.arn
+}
+
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "EC2InstanceProfile"
   role = aws_iam_role.jenkins_ec2_role.name
